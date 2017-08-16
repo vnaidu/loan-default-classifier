f079a07c7b23241d327e1759a077f51d1984c3fe
Victor Naidu <victor@vnaidu.com>
2017-08-15 20:21:38 -0700

    Update Git Pages

---
+++ docs/_build/_static/js/custom.js
@@ -0,0 +1,7 @@
+// Shorthand for $( document ).ready()
+$(function() {
+    $("table").addClass('table table-striped table-hover');
+    $(".highlight-ipython3").addClass('zoom');
+    $(".section ol p").addClass('zoom');
+    $(".section ul p").addClass('zoom');
+});
