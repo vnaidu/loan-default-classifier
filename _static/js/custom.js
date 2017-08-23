0c986d593bfe6267e2f4bc3af654274530d19503
Victor Naidu <victor@vnaidu.com>
2017-08-23 14:40:42 -0700

    Update Docs from Master

---
+++ _static/js/custom.js
@@ -0,0 +1,10 @@
+// Shorthand for $( document ).ready()
+$(function() {
+    $("table").addClass('table table-striped table-hover');
+    $(".highlight-ipython3").addClass('zoom');
+    $(".section ol p").addClass('zoom');
+    $(".section ul p").addClass('zoom');
+    $("li.toctreel1 > a").click(function() {
+      $( this ).children( "span.toctree-expand").click();
+    });
+});
